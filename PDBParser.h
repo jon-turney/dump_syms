7d91eb188aabd32a44e97984ca2340753f45d270
Ted Mielczarek <ted@mielczarek.org>
2014-09-26 10:09:09 -0400

    first cut at omap support

--- PDBParser.h
+++ PDBParser.h
@@ -286,6 +286,7 @@ private:
 	// If we decide to only support VC2013 we can use this.
 	//template<typename T>
 	//using FPODataMap = std::map<std::pair<uint32_t, uint32_t>, DataPtr<T>>;
+	typedef std::vector<Omap> Omaps;
 
 	struct NameStream
 	{
@@ -314,9 +315,11 @@ private:
 
 	void printHeader(const DBIHeader* header, FILE* of, const char* platform = nullptr);
 	void readSectionHeaders(uint32_t headerStream, SectionHeaders& headers);
+	void readOmaps(uint32_t omapStream, Omaps& omaps);
 	void getModuleFiles(const DBIModuleInfo* module, uint32_t& id, UniqueSrcFiles& unique, SrcFileIndex& fileIndex);
 	void printFiles(const SrcFileIndex& fileIndex, FILE* of);
-	void getModuleFunctions(const DBIModuleInfo* module, Functions& funcs);
+	void getModuleFunctions(const DBIModuleInfo* module, const Omaps& omaps, Functions& funcs);
+	void remapFunction(const Omaps& omaps, FunctionRecord& rec);
 	void getGlobalFunctions(uint16_t symRecStream, const SectionHeaders& headers, Functions& globals);
 	void resolveFunctionLines(const DBIModuleInfo* module, Functions& funcs, const UniqueSrcFiles& unique, const SrcFileIndex& fileIndex);
 	void printFunctions(Functions& funcs, const TypeMap& tm, FILE* of);
